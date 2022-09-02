b184d43a5158718ffc31dd4ead587aef7cbdb333
David Pyle <dmpyle@gmail.com>
2022-09-02 14:44:48 -0400

    first commit

---
+++ basic_template.R
@@ -0,0 +1,19 @@
+rm(list = ls())
+
+source("dmp_tidyverse.R")
+
+#### Designate filepaths and load files####
+user.path <- Sys.getenv("HOME")
+
+#checking that local dropbox folder is in user directory or searching 1 layer of subdirectories to find it
+if(!dir.exists(local.db.path <- file.path(user.path, "Dropbox (Partners HealthCare)"))) {
+  for (subdir in list.dirs("~", recursive = F)) {
+    if(dir.exists(file.path(subdir, "Dropbox (Partners HealthCare)"))) {
+      local.db.path <- file.path(subdir, "Dropbox (Partners HealthCare)")
+      break
+    }
+  }
+  rm(subdir)
+}
+
+#### *** ####
