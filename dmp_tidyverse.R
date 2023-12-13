993b066791ba2270bb306fa83c7ca5dd2f4382c1
David Pyle <dmpyle@gmail.com>
2023-12-13 10:37:18 -0500

    update package ordering

--- dmp_tidyverse.R
+++ dmp_tidyverse.R
@@ -1,17 +1,18 @@
+#### Script to install and load Tidyverse packages ####
 
 #loads tidyverse packages and installs if not found
 while(!requireNamespace("tidyverse", quietly = T)) install.packages("tidyverse")
 
 #attaches tidyverse packages and commonly used supplements
 library(tidyverse) #readr, tidyr, dplyr, tibble, stringr, forcats, purrr, ggplot2
-library(magrittr, pos = "package:tidyverse")
+library(magrittr)
+library(rlang)
+library(modelr, pos = "package:tidyverse")
 library(glue, pos = "package:tidyverse")
-library(broom, pos = "package:tidyverse")
 library(lubridate, pos = "package:tidyverse")
 library(readxl, pos = "package:tidyverse")
-library(modelr, pos = "package:tidyverse")
+library(broom, pos = "package:tidyverse")
 library(reprex, pos = "package:tidyverse")
-library(rlang, pos = "package:tidyverse")
 
 message(
 "Installed (if necessary) and loaded tidyverse packages (ggplot2, tibble, tidyr, readr, purrr, dplyr, stringr, forcats), 
